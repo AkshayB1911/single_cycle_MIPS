c6939c08c485089270782fb9124e23005ea090e8
AkshayB1911 <143801065+AkshayB1911@users.noreply.github.com>
2025-06-30 15:50:38 +0530

    Create d_flip_flop.v

---
+++ d_flip_flop.v
@@ -0,0 +1,11 @@
+module d_flip_flop (
+    input wire d,
+    input wire clk,
+    input wire rst,
+    output reg q
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst) q <= 1'b0;
+        else q <= d;
+    end
+endmodule
